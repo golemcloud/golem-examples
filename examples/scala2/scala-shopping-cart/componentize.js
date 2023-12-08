a9ebba4e952257240034f64f52a88d39d0e4cbe7
Jorge Vasquez <jorge.vasquez0123@proton.me>
2023-12-08 09:31:51 -0400

    Add Scala 2 template

---
+++ examples/scala2/scala-shopping-cart/componentize.js
@@ -0,0 +1,13 @@
+import { componentize } from '@bytecodealliance/componentize-js';
+import { readFile, writeFile } from 'node:fs/promises';
+import { resolve } from 'node:path';
+
+const jsSource = await readFile('target/dist/main.js', 'utf8');
+
+const { component } = await componentize(jsSource, {
+  witPath: resolve('wit'),
+  enableStdout: true,
+  preview2Adapter: 'adapters/tier2/wasi_snapshot_preview1.wasm'
+});
+
+await writeFile('target/dist/component-name.wasm', component);
