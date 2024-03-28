97f809cfdee4b54f2bb0af9c74b58b8611c4294e
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2024-03-28 07:17:29 +0100

    Updated adapter paths

--- examples/js/js-actor-minimal/componentize.js
+++ examples/js/js-actor-minimal/componentize.js
@@ -1,14 +1,14 @@
-import { componentize } from '@bytecodealliance/componentize-js';
-import { readFile, writeFile, mkdir } from 'node:fs/promises';
-import { resolve } from 'node:path';
+import { componentize } from "@bytecodealliance/componentize-js";
+import { readFile, writeFile, mkdir } from "node:fs/promises";
+import { resolve } from "node:path";
 
-const jsSource = await readFile('main.js', 'utf8');
+const jsSource = await readFile("main.js", "utf8");
 
 const { component } = await componentize(jsSource, {
-  witPath: resolve('wit'),
+  witPath: resolve("wit"),
   enableStdout: true,
-  preview2Adapter: 'adapters/tier2/wasi_snapshot_preview1.wasm'
+  preview2Adapter: "adapters/tier1/wasi_snapshot_preview1.wasm",
 });
 
-await mkdir('out', { recursive: true });
-await writeFile('out/component_name.wasm', component);
+await mkdir("out", { recursive: true });
+await writeFile("out/component_name.wasm", component);
