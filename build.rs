446251d1d57670a3e21eaf5c4f4884efaf2840b1
Marijan Petričević <marijan.petricevic94@gmail.com>
2024-09-06 17:11:24 +0200

    build.rs: make wit root configurable with an env-var

--- build.rs
+++ build.rs
@@ -5,7 +5,9 @@ use std::path::PathBuf;
 
 fn main() {
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
-    let golem_wit_root = PathBuf::from(find_package_root("golem-wit"));
+    let golem_wit_root = PathBuf::from(
+        env::var("GOLEM_WIT_ROOT").unwrap_or_else(|_| find_package_root("golem-wit")),
+    );
 
     println!("cargo:warning=Output dir: {out_dir:?}");
     println!("cargo:warning=Golem WIT root: {golem_wit_root:?}");
